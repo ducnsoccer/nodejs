b1bed601fa66ed06e4545e084a135eda4ade5109
Hoai Nguyen <hn003f@att.com>
2020-04-27 08:46:59 -0700

    Event-Driven Architecture, Blocking vs Non-Blocking

--- demo01/app.js
+++ demo01/app.js
@@ -12,20 +12,24 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+
     if (url === '/message' && method === 'POST') {
         const body = [];
+
         req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
